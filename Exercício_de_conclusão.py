965477626dfe5e115769e0257c678428b82d0074
ARTHUR ATHAYDE PEIXOTO <130025668+pwgathayde@users.noreply.github.com>
2023-11-19 20:55:44 -0300

    Update Exercício_de_conclusão.py

--- Exercício_de_conclusão.py
+++ Exercício_de_conclusão.py
@@ -1,2 +1,25 @@
+#Sobre o projeto: Encontrar uma problemática da vida real que possa ser solucionada
+#através de análise de dados e machine learning. A ideia do projeto é
+#explanar e justificar a relevância do uso de dados para encontrar a
+#solução. Você irá: Dissertar sobre o problema;
+#•  Dissertar sobre o problema;
+#•  Levantar as fontes de dados públicas e não confidenciais para a
+#   coleta de informações.
+   
+#•  Efetuar a análise exploratória de dados levantando os principais
+#pontos relevantes para a solução do problema.
+
+#•  Construir um aprendizado de máquina ou modelo estatístico e
+#  avaliar através de técnicas de modelagem.
+#•  Apresentar uma visualização de dados com os resultados
+#  obtidos.
+#Módulos com informações importantes sobre o projeto: 
+#• Módulo 22: Aprendizado de Máquina Fundamentos
+#• Módulo 23: Aprendizado de Máquina Regressão
+#• Módulo 24: Aprendizado de Máquina Agrupamento
+
+# Ideia para o projeto: 
+
+#bibliotecas para se importar:
 import pandas as pd
 
